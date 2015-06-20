d26fd3e8bdd0676619e8e3a5b80f928312d9d93c
osmanbaskaya <obaskaya@ku.edu.tr>
2015-06-20 17:08:30 +0300

    Random baselines (weighted/shuffled) added.

--- run/supervised-random-baseline.py
+++ run/supervised-random-baseline.py
@@ -4,22 +4,29 @@ __author__ = "Osman Baskaya"
 
 import sys
 from nlp_utils import fopen
-from random import randint
+from random import randint, shuffle
 from collections import defaultdict as dd
+from itertools import izip
 
 test_f = sys.argv[1]
+method = sys.argv[2]  # shuffled / weighted
 
 senses = dd(list)
 instances = dd(list)
-observed_senses = set()
 for line in fopen(test_f):
     tw, inst_id, sense = line.split()
     instances[tw].append(inst_id)
-    if sense not in observed_senses:
-        senses[tw].append(sense)
-        observed_senses.add(sense)
+    senses[tw].append(sense)
 
-for tw in sorted(instances.keys()):
-    n = len(senses[tw])
-    for instance in instances[tw]:
-        print "%s %s %s" % (tw, instance, senses[tw][randint(0, n-1)])
+if method == 'weighted':
+    for tw in sorted(instances.keys()):
+        n = len(senses[tw])
+        for instance in instances[tw]:
+            print "%s %s %s" % (tw, instance, senses[tw][randint(0, n-1)])
+elif method == 'shuffled':
+    for tw in sorted(instances.keys()):
+        shuffle(senses[tw])
+        for instance, sense in izip(instances[tw], senses[tw]):
+            print "%s %s %s" % (tw, instance, sense)
+else:
+    print "Need to call %s test_data shuffled/weigted" % sys.argv[0]
