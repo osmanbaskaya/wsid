d30601803a11c28468df86b27994b842dead9b6b
osmanbaskaya <obaskaya@ku.edu.tr>
2015-06-23 00:22:54 +0300

    script that calculate absolute difference between different systems

---
+++ run/absolute-diff.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = "Osman Baskaya"
+
+"""
+Take absolute difference between two list
+"""
+
+import sys
+from itertools import combinations, izip
+import numpy as np
+
+score_field = int(sys.argv[1]) # indicate the score field
+file_comb = list(combinations(sys.argv[2:], 2))
+score_files = map(lambda f: open(f).readlines(), sys.argv[2:])
+score_lists = map(lambda L: map(lambda line: float(line.split()[score_field]), L), score_files)
+score_lists = map(np.array, score_lists)
+score_comb = list(combinations(score_lists, 2))
+
+for (f1, f2), (score1, score2) in izip(file_comb, score_comb):
+    print f1, f2, sum(abs(score1-score2)) / len(score1)
